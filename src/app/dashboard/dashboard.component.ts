844c707b4614ee596ce3e451d02d6b7fd44ce452
Swathi1812Harikumaar <swathihari1812@gmail.com>
2021-05-18 15:01:22 +0530

    chart corrected

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { CoronaserviceService } from 'src/app/coronaservice.service';
-import Chart from 'chart.js/auto';
+import { Component, OnInit } from "@angular/core";
+import { CoronaserviceService } from "src/app/coronaservice.service";
+import Chart from "chart.js/auto";
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css'],
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.css"],
 })
 export class DashboardComponent implements OnInit {
-  title = 'covidreport';
-  countries: string;
+  title = "covidreport";
+  countries: any;
   country: string;
   confirmed: number;
   recovered: number;
@@ -18,17 +18,16 @@ export class DashboardComponent implements OnInit {
 
   constructor(private corona: CoronaserviceService) {}
 
-  currentlyConfirmed: number[] = [];
-  currentlyRecovered: number[] = [];
-  currentlyDeath: number[] = [];
+  currentlyConfirmed: number[];
+  currentlyRecovered: number[];
+  currentlyDeath: number[];
 
   ngOnInit(): void {
     this.corona.getCountries().subscribe((data) => {
       console.log(data);
       this.countries = data;
     });
-    this.getData();
-   // this.map();
+    this.getDailyData();
   }
 
   getCountry(country: string) {
@@ -37,74 +36,55 @@ export class DashboardComponent implements OnInit {
 
   getTheData() {
     this.corona.getCoronaCurrentData(this.country).subscribe((data) => {
-      console.log(data);
-      const index = data.length - 1;
+     //console.log(data[0].Confirmed);
+      var index = data.length - 1;
       this.confirmed = data[index].Confirmed;
       this.recovered = data[index].Recovered;
       this.deaths = data[index].Deaths;
     });
   }
 
-  getDailyData() {
-    this.corona.getCoronaCurrentData(this.country).subscribe((data) => {
-      var index = data.length - 1;
-      this.currentlyConfirmed.push(data[index].Confirmed);
-      this.currentlyRecovered.push(data[index].Deaths);
-      this.currentlyDeath.push(data[index].Recovered);
-    });
-  }
-
   getData() {
     this.getTheData();
-    this.map();
     this.getDailyData();
-
- //  if (this.myChart != undefined) this.myChart.destroy();
-    
   }
-  
 
-  map() {
-    
-    const labels = ['confirmed', 'recovered', 'death'];
-    const data = {
-      labels: labels,
-      datasets: [
-        {
-          label: ' Covid Data',
-          data: [
-            this.currentlyConfirmed,
-            this.currentlyDeath,
-            this.currentlyRecovered,
+  getDailyData() {
+    this.corona.getCoronaCurrentData(this.country).subscribe((data) => {
+  this.currentlyConfirmed = data[data.length - 1]["Confirmed"];
+  this.currentlyRecovered = data[data.length - 1]["Recovered"];
+  this.currentlyDeath = data[data.length - 1]["Deaths"];
+
+  if (this.myChart != undefined) this.myChart.destroy();
+
+      this.myChart = new Chart("myChart", {
+        type: 'bar',
+        data: {
+          labels: ["confirmed", "recovered", "death"],
+          datasets: [
+            {
+              label: "Covid Data",
+              data: [
+                this.currentlyConfirmed,
+                this.currentlyRecovered,
+                this.currentlyDeath,
+              ],
+              backgroundColor: [
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(255, 159, 64, 0.2)",
+                "rgba(255, 205, 86, 0.2)",
+              ],
+              borderColor: [
+                "rgb(255, 99, 132)",
+                "rgb(255, 159, 64)",
+                "rgb(255, 205, 86)",
+              ],
+              borderWidth: 1,
+            },
           ],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
-            'rgba(255, 205, 86, 0.2)',
-          ],
-          borderColor: [
-            'rgb(255, 99, 132)',
-            'rgb(255, 159, 64)',
-            'rgb(255, 205, 86)',
-          ],
-          borderWidth: 1,
         },
-      ],
-    };
-    if (this.myChart != undefined) this.myChart.destroy();
-    this.myChart = new Chart('myChart', {
-      type: 'bar',
-      data: data,
-      options: {
-        scales: {
-          x: {
-            stacked: true,
-          },
-          y: {
-            stacked: true,
-          },
-        },
-      },
+      })
     });
   }
+
 }
